fb9962dfcb2682d38310f7ccee5b338430ce9eb3
dertyui2okijwuhgf <134987392+dertyui2okijwuhgf@users.noreply.github.com>
2023-05-31 02:24:40 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'c664751b-b662-48f9-b2aa-2f0d4d337a7f'}
+UUID=${UUID:-'fb203890-adf8-49f9-8ace-bdb5dac12b1f'}
 
 # 其他Paas保活
 PAAS1_URL=
@@ -17,8 +17,8 @@ KOYEB_ACCOUNT=
 KOYEB_PASSWORD=
 
 # Argo 固定域名隧道的两个参数,这个可以填 Json 内容或 Token 内容，获取方式看 https://github.com/fscarmen2/X-for-Glitch，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-ARGO_AUTH=''
-ARGO_DOMAIN=
+ARGO_AUTH='{"AccountTag":"61c220d2e5bf89441da6786c6496fe24","TunnelSecret":"ZuZGN7IegowRFkE/F9Fz7FzqZGbWj1YFFEJOArV/ico=","TunnelID":"1f1dcb4c-cef5-400e-9bbd-34132ea83eec"}'
+ARGO_DOMAIN=back4.verhrvhrbrg.ga
 
 generate_config() {
   cat > config.json << EOF
